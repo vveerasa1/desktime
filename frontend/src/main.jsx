828d837fcc15153f53e01b423ff97b0ed6e36101
vveerasa1 <vijayram.veeraswamy@gmail.com>
2025-06-14 15:48:54 +0530

    Redux Toolkit Setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import { store } from './redux/store/store.js'
 createRoot(document.getElementById('root')).render(
+<Provider store={store}>
   <StrictMode>
     <App />
   </StrictMode>,
+</Provider>
+  
 )
