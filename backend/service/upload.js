951a1f85970f37ddb9924fcbed66301389b795f2
Vijay <vijayram.veeraswamy@gmail.com>
2025-07-02 14:11:57 +0530

    Upload image

--- backend/service/upload.js
+++ backend/service/upload.js
@@ -1,6 +1,8 @@
 const AWS = require("aws-sdk");
 
 const s3 = new AWS.S3({
+  accessKeyId: config.AWS.ACCESS_KEY_ID,
+  secretAccessKey: config.AWS.SECRET_ACCESS_KEY,
   region: 'ca-central-1',
   ACL: "public-read",
 });
